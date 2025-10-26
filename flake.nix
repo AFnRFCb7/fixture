4f8eae899c9c0a083095556a9ea19e59642b1b55
Emory Merryman <emory.merryman@gmail.com>
2025-10-26 19:47:32 -0400

    fixture

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
                                                     writeShellApplication
                                                         {
                                                             name = "execute-fixture" ;
-                                                            runtimeInputs = [ age coreutils gnupg libuuid ( failure "0118ba19" ) ] ;
+                                                            runtimeInputs = [ age coreutils gnupg libuuid ( failure.implementation "0118ba19" ) ] ;
                                                             text =
                                                                 ''
                                                                     OUT="$1"
